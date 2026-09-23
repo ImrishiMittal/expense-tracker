f26a9d6a275d9cbb036e5362e92a405318b4ef70
Rishi Mittal <rishi11mital@gmail.com>
2026-09-23 17:28:47 +0530

    Initial Expo project setup

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 50 }}>
+      HELLO RISHI !!
+    </Text>
+  );
+}
